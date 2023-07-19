edc66db0cc826fe43a83e5365eddeb1a5491112f
Lulu <lulu.luo@arionlabs.com.tw>
2023-07-19 16:32:31 +0800

    fix: logical error

--- numericRange.js
+++ numericRange.js
@@ -1,38 +1,52 @@
-function findOverlapAndNotInclude(intervals) {
-    const overlapIntervals = [];
-    const notIncludeIntervals = [];
-    const allIntervals  = intervals.map(interval => Array.isArray(interval) ? interval : [interval]);
-  
-    allIntervals.sort(function(a, b) {
-      return a[0] - b[0];
-    });
-  
-    let currentEnd = 0;
-    for (let i = 0; i < allIntervals.length; i++) {
-      const currentInterval = allIntervals[i];
-      const start = currentInterval[0];
-      const end = currentInterval.length > 1 ? currentInterval[1] : currentInterval[0];
-  
-      if (start <= currentEnd) {
-        overlapIntervals.push([start, Math.min(end, currentEnd)]);
-        currentEnd = Math.max(end, currentEnd);
-      } else {
-        notIncludeIntervals.push([currentEnd + 1, start - 1]);
-        currentEnd = end;
-      }
+const getOutput = (result) => {
+  let output = [];
+  let startPoint = null;
+  result.forEach((val, index) => {
+    if (!startPoint) {
+      startPoint = val;
     }
-  
-    if (currentEnd < 20) {
-      notIncludeIntervals.push([currentEnd + 1, 20]);
+    const isEnd = result[index + 1] !== val + 1;
+    if (isEnd) {
+      let endPoint = val;
+      output = [
+        ...output,
+        startPoint !== endPoint ? [startPoint, endPoint] : [endPoint],
+      ];
+      startPoint = null;
     }
-  
-    return {
-      overlap: overlapIntervals,
-      notInclude: notIncludeIntervals
-    };
-  }
-  
-  const input = [[6, 11], [5, 8], [17, 20], [7], [14, 17]];
-  const output = findOverlapAndNotInclude(input);
-  
-  console.log(output);//{ overlap: [[6, 8], [17]], notInclude: [[1, 4], [12, 13]] }
+  });
+
+  return output;
+};
+
+
+const findOverlapAndNotInclude = (input = []) => {
+  const includedNumbers = Array.from({ length: 20 }, (_, i) => i + 1).map(
+    () => 0
+  );
+  input.forEach((numberArr = []) => {
+    const start = Math.min(...numberArr);
+    const end = Math.max(...numberArr);
+
+    for (let num = start; num <= end; num++) {
+      includedNumbers[num - 1]++;
+    }
+  });
+
+  const overlap = includedNumbers
+    .map((item, index) => (item > 1 ? index + 1 : false))
+    .filter(Boolean);
+
+  const notInclude = includedNumbers
+    .map((item, index) => (item === 0 ? index + 1 : false))
+    .filter(Boolean);
+  return {
+    overlap: getOutput(overlap),
+    notInclude: getOutput(notInclude),
+  };
+};
+
+const input = [[6, 11], [5, 8], [17, 20], [7], [14, 17]];
+const output = findOverlapAndNotInclude(input);
+
+console.log("output", output);
